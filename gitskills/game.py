3da07180f433755acb4702fee2166c872dc84aca
Karl Wu <wuchanghui5220@gmail.com>
2019-11-18 16:57:04 +0800

    modified

---
+++ gitskills/game.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+a = 7
+c = 3
+print("Welcome to guess game!")
+while c > 0:
+        number = int(input("Please input a number: ")
+	if number > a:
+		print("Too Big")
+	elif number < a:
+		print("Too small")
+	else:
+		print("Yes, you did it")
+		break
+	c -= 1
+	print("You have " + str(c) + " chances left. ")
+print("Have Fun")
+
+print("^_^" * 3)
+
+# This game is Fun.
